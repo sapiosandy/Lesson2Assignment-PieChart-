a6ce5147ef8a85704c4b199c470f3dc2ee2a74c2
Sandy Gomez <sgomez1029@gmail.com>
2024-04-11 14:22:05 -0700

    Lesson 2 Assignment (Part 2 - Pie Chart)

--- Lesson2Assignment(PieChart)/ContentView.swift
+++ Lesson2Assignment(PieChart)/ContentView.swift
@@ -4,18 +4,34 @@
 //
 //  Created by Sandra Gomez on 4/11/24.
 //
-
 import SwiftUI
+import Charts
+
+struct FridayStage: Identifiable {
+    var stage: String
+    var votes: Int
+    var id: String {stage}
+}
+
+var myFridayArray: [FridayStage] = [
+    .init(stage: "Techno", votes: 8),
+    .init(stage: "HardStyle", votes: 4),
+    .init(stage: "Drum & Bass", votes: 3),
+ 
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Chart(myFridayArray) { z in
+            SectorMark(
+                angle: .value("Sales", z.votes),
+                angularInset: 2.0            )
+            .foregroundStyle(by: .value("Stage", z.stage))
+            .annotation(position: .overlay) {
+                Text ("\(z.votes)")
+                    .foregroundStyle(.white)
+            }
+        } .padding()
     }
 }
 
