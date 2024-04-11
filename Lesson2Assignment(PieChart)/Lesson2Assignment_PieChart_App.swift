94d9b99f864f52f69a8bbf47150736c9cb03383e
Sandy Gomez <sgomez1029@gmail.com>
2024-04-11 14:10:28 -0700

    Initial Commit

---
+++ Lesson2Assignment(PieChart)/Lesson2Assignment_PieChart_App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson2Assignment_PieChart_App.swift
+//  Lesson2Assignment(PieChart)
+//
+//  Created by Sandra Gomez on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Lesson2Assignment_PieChart_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
